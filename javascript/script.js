ffb4cbaad53910b48fcfbecdc007b4966f485ed8
Maia Araújo <jessicamaiaaraujo@gmail.com>
2023-09-15 13:37:20 -0300

    seções atualizadas

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll(".navbar a");
+
+    links.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
